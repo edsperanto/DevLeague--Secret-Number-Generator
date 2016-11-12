a09ab2d963a8d04d063254718f8f5df68610f229
Edward Gao <egao@sent.com>
2016-11-12 12:10:20 -1000

    it works

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,10 @@
 'use strict';
 module.exports = function() {
-  
+  var _rand = Math.floor(Math.random() * 1000001);
+
+  function secretNumber() {
+    return  _rand;
+  }
+
+  return secretNumber;
 };
